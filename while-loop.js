689eaf515b88f103d3e29df73754fc62e45f6ca8
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-12-17 01:10:37 +0600

    while loop

---
+++ while-loop.js
@@ -0,0 +1,40 @@
+// let rostGiven = 0;
+// while (rostGiven < 7) {
+//   console.log(rostGiven);
+//   console.log("Rost dan Please");
+//   rostGiven++;
+// }
+
+// let number = 1;
+// while (number <= 10) {
+//   console.log(number);
+//   number++;
+// }
+
+// let oddNumber = 1;
+// while (oddNumber <= 10) {
+//   console.log(oddNumber);
+//   oddNumber = oddNumber + 2;
+// }
+
+// let evenNumber = 0;
+// while (evenNumber <= 10) {
+//   console.log(evenNumber);
+//   evenNumber = evenNumber + 2;
+// }
+
+///////////////// Practice again /////////////////
+
+let oddNumber = 1;
+while (oddNumber <= 10) {
+  console.log(oddNumber);
+  oddNumber = oddNumber + 2;
+}
+
+console.log("------- Even Number ---------");
+
+let evenNumber = 0;
+while (evenNumber <= 10) {
+  console.log(evenNumber);
+  evenNumber = evenNumber + 2;
+}
