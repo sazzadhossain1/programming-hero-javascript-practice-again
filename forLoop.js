ef74254507bfe6640ad1dd1ebd3cc1186c0e804e
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-12-17 01:36:55 +0600

    for loop

---
+++ forLoop.js
@@ -0,0 +1,31 @@
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+for (let i = 1; i <= 10; i = i + 2) {
+  console.log("Number", " ", i);
+}
+
+for (let i = 0; i <= 10; i = i + 2) {
+  console.log("This is Even Number", i);
+}
+
+const numbers = [45, 22, 34, 45, 33, 23, 21];
+
+let n = 0;
+while (n <= numbers.length) {
+  console.log(numbers[n]);
+  n++;
+}
+
+console.log("------- For Loops -------");
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
+const items = ["bottle", "mobile", "charger", "lighter"];
+for (let i = 0; i < items.length; i++) {
+  console.log(items[i]);
+}
+
+// 18.5 number module ses 19 number module start
