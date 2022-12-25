9f9eacccad3c30f642198d176114cd5eb7b0822e
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-12-25 16:27:34 +0600

    event listner done

--- event/event.js
+++ event/event.js
@@ -12,4 +12,41 @@ green.onclick = makeGreen;
 function makeGreen() {
   document.body.style.backgroundColor = "green";
 }
-// 25 number module er 2 number ta ses 3 number ta start
+
+const purple = document.querySelector(".purple");
+purple.onclick = function () {
+  document.body.style.backgroundColor = "purple";
+};
+
+///////////////////////////////////////////////////////
+
+// using function
+function prgChangeTestFunction() {
+  const p = document.querySelector("#clicking-method");
+  p.innerText = "Set by using function";
+}
+
+// direct
+const handleEvent = document.querySelector("#handle-event");
+handleEvent.addEventListener("click", function () {
+  const p = document.querySelector("#clicking-method");
+
+  p.innerText = "Set by direct event listener";
+});
+
+/////////////////////////////////////////////////////////////
+
+const updateBtn = document.querySelector("#updateBtn");
+updateBtn.addEventListener("click", function () {
+  let input = document.querySelector("input");
+  let getValue = input.value;
+  console.log(getValue);
+
+  const getp = document.querySelector(".getp");
+  console.log(getp);
+
+  getp.innerText = getValue;
+  input.value = "";
+});
+
+// 25 number module er 4 number ta ses 5 number ta start
